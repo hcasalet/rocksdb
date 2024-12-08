b229eb13b12f213b43fb7b7bcf51920d580d30d1
Holly Casaletto <hcasalet@ucsc.edu>
2024-12-07 21:46:21 -0800

    convert json

--- transformer/converter.cc
+++ transformer/converter.cc
@@ -15,93 +15,40 @@ void Converter::Transform(std::string input, std::vector<std::string>* outputs, 
         input = input.substr(0, end+1);
     }
  
+    flatbuffers::FlatBufferBuilder builder;
+    std::vector<int32_t> numvals;
+    std::vector<flatbuffers::Offset<flatbuffers::String>> strvals;
     switch (converterData->in_type) {
         case InputOutputDataType::PROTOBUF: {
             data::Row row;
             row.ParseFromString(input);
 
-            flatbuffers::FlatBufferBuilder builder;
-            if (converterData->column_data_type == 1) {
-                std::vector<int32_t> numvals;
-                for (int i = 0; i < row.columns_size(); i++) {
-                    numvals.push_back(std::stoi(row.columns(i)));
+            for (int i = 0; i < row.columns_size(); i++) {
+                const std::string& column = row.columns(i); // Avoid repeated lookups
+
+                if (column.empty()) { // Handle empty strings explicitly
+                    strvals.push_back(builder.CreateString(column));
+                    continue;
                 }
 
-                auto num_vector = builder.CreateVector(numvals);
-                auto fb_row_num = rocksdb::CreateFbRowNum(builder, num_vector);
-
-                builder.Finish(fb_row_num);
-            } else if (converterData->column_data_type == 2) {
-                std::vector<flatbuffers::Offset<flatbuffers::String>> string_vector;
-                for (int i = 0; i < row.columns_size(); i++) {
-                    string_vector.push_back(builder.CreateString(row.columns(i)));
+                try {
+                    numvals.push_back(std::stoi(column));
+                } catch (...) {
+                    strvals.push_back(builder.CreateString(column));
                 }
-
-                auto col_vector = builder.CreateVector(string_vector);
-                auto fb_row_str = rocksdb::CreateFbRowStr(builder, col_vector);
-
-                builder.Finish(fb_row_str);
-            } else {
-                std::vector<int32_t> numvals;
-                std::vector<flatbuffers::Offset<flatbuffers::String>> string_vector;
-                for (int i = 0; i < row.columns_size(); i++) {
-                    if (i < row.columns_size()/2) {
-                        numvals.push_back(std::stoi(row.columns(i)));
-                    } else {
-                        string_vector.push_back(builder.CreateString(row.columns(i)));
-                    }
-                }
-
-                auto num_vector = builder.CreateVector(numvals);
-                auto col_vector = builder.CreateVector(string_vector);
-                auto fb_row = rocksdb::CreateFbRow(builder, num_vector, col_vector);
-
-                builder.Finish(fb_row);
             }
-            
-            uint8_t *buf = builder.GetBufferPointer();
-            int size = builder.GetSize();
-            std::string s(reinterpret_cast<char*>(buf), size);
-            outputs->push_back(s);
-
             break;
         }
         case InputOutputDataType::JSON: {
             nlohmann::json parsedJson = nlohmann::json::parse(input);
-            flatbuffers::FlatBufferBuilder builder;
 
-            std::vector<int32_t> numvals;
-            numvals.push_back(parsedJson["field0"].get<int>());
-            numvals.push_back(parsedJson["field1"].get<int>());
-            numvals.push_back(parsedJson["field2"].get<int>());
-            numvals.push_back(parsedJson["field3"].get<int>());
-            numvals.push_back(parsedJson["field4"].get<int>());
-            numvals.push_back(parsedJson["field5"].get<int>());
-            numvals.push_back(parsedJson["field6"].get<int>());
-            numvals.push_back(parsedJson["field7"].get<int>());
-            auto num_vector = builder.CreateVector(numvals);
-
-            std::vector<flatbuffers::Offset<flatbuffers::String>> strvals;
-            strvals.push_back(builder.CreateString(parsedJson["field8"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field9"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field10"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field11"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field12"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field13"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field14"].get<std::string>()));
-            strvals.push_back(builder.CreateString(parsedJson["field15"].get<std::string>()));
-            auto col_vector = builder.CreateVector(strvals);
-
-            auto fbRow = rocksdb::CreateFbRow(
-                builder, num_vector, col_vector
-            );
-            builder.Finish(fbRow);
-
-            uint8_t *buf = builder.GetBufferPointer();
-            int size = builder.GetSize();
-            std::string s(reinterpret_cast<char*>(buf), size);
-            outputs->push_back(s);
-
+            for (const auto& element : parsedJson) {
+                if (element.is_number()) {
+                    numvals.push_back(element.get<int>());
+                } else if (element.is_string()) {
+                    strvals.push_back(builder.CreateString(element.get<std::string>()));
+                }
+            }
             break;
         }
         default: {
@@ -109,6 +56,16 @@ void Converter::Transform(std::string input, std::vector<std::string>* outputs, 
             break;
         }
     }
+
+    auto num_vector = builder.CreateVector(numvals);
+    auto col_vector = builder.CreateVector(strvals);
+    auto fb_row = rocksdb::CreateFbRow(builder, num_vector, col_vector);
+    builder.Finish(fb_row);
+            
+    uint8_t *buf = builder.GetBufferPointer();
+    int size = builder.GetSize();
+    std::string s(reinterpret_cast<char*>(buf), size);
+    outputs->push_back(s);
     
     return;
 }
